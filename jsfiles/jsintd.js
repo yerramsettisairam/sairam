90ca5ae30e0d90edfc425f57f8044631ca6dcc9d
yerramsetti sairam <yerramsettinaveen0644@gmail.com>
2021-08-12 11:11:19 +0530

    update the file in js

---
+++ jsfiles/jsintd.js
@@ -0,0 +1,9 @@
+function jsintroduction(){
+document.getElementById("a").innerHTML="some dummy text don't read";
+alert("hii frnds");
+    
+}
+//alert("hii frnds");
+//document.getElementById("a").innerHTML="some dummy text don't read";
+//document.write("some text is here");
+//console.log("some text");
